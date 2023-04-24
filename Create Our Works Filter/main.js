89de879bfa531059d1e641a702ad2a00364bd1dd
amarmedmoctar <amarmed4500@gmail.com>
2023-04-24 19:32:40 +0100

    Create Our Works Filter project

---
+++ Create Our Works Filter/main.js
@@ -0,0 +1,26 @@
+let switcherLis = document.querySelectorAll(".switcher li");
+let imgs = Array.from(document.images);
+
+switcherLis.forEach((li) => {
+  li.addEventListener("click", removeActive);
+  li.addEventListener("click", manageImgs);
+});
+
+// Remove Active Class From All Lis And Add to current
+
+function removeActive() {
+  switcherLis.forEach((li) => {
+    li.classList.remove("active");
+    this.classList.add("active");
+  });
+}
+
+// Manage Imgs
+function manageImgs() {
+  imgs.forEach((img) => {
+    img.style.display = "none";
+  });
+  document.querySelectorAll(this.dataset.cat).forEach((el) => {
+    el.style.display = "block";
+  });
+}
