adaef2faf13595ffb05ff7d5e4ba9c42b98d7869
amarmedmoctar <amarmed4500@gmail.com>
2023-04-29 02:16:03 +0000

    End LocalStorage App

--- LocalStrorage & App/main.js
+++ LocalStrorage & App/main.js
@@ -1,5 +1,47 @@
-localStorage.setItem("Persone", "Amar"); // Add key and val to local storage
-localStorage.setItem("Language", "Franch")
-localStorage.setItem("Language", "English")
+//Cash Vars
 
-console.log(localStorage.setItem("Language", "Franch"))
+const form = document.querySelector("form");
+const lul = document.querySelector("ul");
+const input = document.querySelector("item");
+const ClearButton = document.querySelector("button");
+const items = JSON.parse(localStorage.getItem("tasks"))
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
+const data = JSON.parse(localStorage.getItem("tasks"))
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
+
+//Create li and append to ul
+
+const createli = (text) => {
+  const li = document.createElement("li");
+  li.textContent = text;
+  ul.appendChild(li);
+};
+
+// oop through the data
+
+data.map((item) => {
+  createli(item);
+});
+
+//SubmitFormEvnt
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // add to lacalStorage
+  items.push(input.value);
+  localStorage.setItem("tasks", JSON.stringify(items));
+
+  // add to ul
+  createli(input.value);
+  input.value = "";
+});
+
+//ClearData
+
+ClearButton.addEventListener("submit", function () {
+  localStorage.clear();
+  ul.innerHTML = "";
+});
