0382a022590daf05db1e4eb3ed91a9372488e32b
amarmedmoctar <amarmed4500@gmail.com>
2023-04-30 11:38:53 +0000

    Auto-Write-Text

---
+++ auto-write-text/main.js
@@ -0,0 +1,16 @@
+const text = "Knowing how to motivate yourself can help you accomplish anything you set your mind to";
+
+let index = 0 ;
+
+function writeText(){
+  document.body.innerText = text.slice(0, index);
+
+  index++;
+
+  if(index > text.length - 1){
+    index = 0;
+  }
+
+}
+
+setInterval(writeText, 100)
